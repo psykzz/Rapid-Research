0beb14d92a07e549a3af20e55949ea69bd410019
PsyKzz <matt.daemon660@gmail.com>
2016-07-11 02:58:04 +0100

    Add first steps of an install script

---
+++ install.bat
@@ -0,0 +1,2 @@
+echo d | xcopy /S /Y "rapidresearch" "%appdata%\..\..\Documents\Paradox Interactive\Stellaris\mod\rapidresearch"
+echo f | xcopy /Y "rapidresearch.mod" "%appdata%\..\..\Documents\Paradox Interactive\Stellaris\mod\rapidresearch.mod"
