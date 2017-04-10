dab168a2285d79900883eed021353a22ba41a79c
PsyKzz <matt.daemon660@gmail.com>
2017-04-10 19:19:50 +0100

    Update to v1.5.0

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 echo d | xcopy /S /Y "rapidresearch" "%appdata%\..\..\Documents\Paradox Interactive\Stellaris\mod\rapidresearch"
 echo f | xcopy /Y "rapidresearch.mod" "%appdata%\..\..\Documents\Paradox Interactive\Stellaris\mod\rapidresearch.mod"
+pause
